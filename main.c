4e5ef3764dd4a81be6a33ab69991d1e1ce507941
WATDANAI SANSENAKULLACHOTE <122853666+WatdanaiS@users.noreply.github.com>
2023-02-03 09:17:50 +0000

    test 3 feb 2023

--- main.c
+++ main.c
@@ -4,25 +4,27 @@
 
 int main(int argc ,char **argv) {
 int i,*a,n,j=0;
-int *prime=(int*)malloc(sizeof(int)*n);
+//int *prime=(int*)malloc(sizeof(int)*n);
   n=argc-1;
   a=(int*)malloc(sizeof(int)*n);
   for(i=0;i<n;i++) {
     a[i]=atoi(argv[i+1]);
-      if(IsPrime(a[i])){
+  /*    if(IsPrime(a[i])){
         prime[j]=a[i];
         j++;
-      }
+      }*/
     }
 
-  n=j;
-  if(n==0) printf("No prime\n");
-  else{
-   display(prime,n);
-  bubbleSort(prime,n); 
-   //insertion(a,N);
-  // selectionSort(a,N);
-  display(prime,n);
-}
+  //n=j;
+  //if(n==0) printf("No prime\n");
+  //else{
+    //display(prime,n);
+    //bubbleSort(a,n);
+    //selectionSort(a,n);
+    insertion(a,n);
+    display(a,n);
+    
+  
+//}
   return 0;
 }
--- sorting.h
+++ sorting.h
@@ -11,23 +11,47 @@ void display(int a[],int n){
 }
 
 void selectionSort(int a[], int l){ 
-	int i, j, t, m, mi;
+	int i, j, m, mi;
     // display(a,l);
-  for(i=0;i<l;i++){
-    if(a[i]<a[i+1]){
-      
+  
+  for(j=0;j<l-1;j++){
+    m=a[j];
+    mi=j;
     }
+  for(i=j+1;i<l;i++){
+    if(m>a[i]){
+      m=a[i];
+      mi=i;
+    }
+    
   }
+  swap(&a[j],&a[mi]);
+  printf("Min is %d\n",m);
      
 	
 } 
 
 
 
-void insertion(int a[],int n){
-
-
+void insertion(int x[],int n){
+  
+  int j, i;
+  for(j=1;j<n;j++){
+  int insert=x[j];
+  for(i=j-1;i>=0;i--){
+    if(insert>x[i])
+      break;
+    else
+      x[i+1]=x[i];
+    display(x,n);
+    }
+  x[i+1]=insert;
+    display(x,n);
+    printf("\n");
+  }
 }
+
+
 
 void swap(int *a,int *b){
  int temp;
@@ -48,7 +72,7 @@ for(i=0;i<n-1;i++){
      swap(&a[j],&a[j+1]);
       sorted=0;
      }
-     display(a,n);
+     //display(a,n);
     }
   //if(sorted==0) break;
   }
